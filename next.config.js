6f4284de09370881f7fee74746f708d990946551
Jasper Chesselet <jasper.chesselet@zappistore.com>
2023-05-08 21:01:41 +0200

    move to pages dir

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-      typedRoutes: true,
-      serverComponentsExternalPackages: ["mysql2"],
-    },
 }
 
 module.exports = nextConfig
---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { Inter } from 'next/font/google'
+
+import "./globals.css"
+import Head from "next/head";
+
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+   
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+    title: 'Tech Tinkerers Twitter',
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    return (
+        <>
+        <Head>
+            <title>{metadata.title}</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+            <Component {...pageProps} />
+        </>
+    );
+  }
