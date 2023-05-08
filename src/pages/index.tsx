b1574d5c72027067e3b86650b8b74e84d0ad4504
Jasper Chesselet <jasper.chesselet@zappistore.com>
2023-05-08 21:24:04 +0200

    remove SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { getConnection } from "../connection";
-
 interface Post {
   id: number;
   text: string;
@@ -7,29 +5,10 @@ interface Post {
   created_at: string;
 }
 
-export async function getServerSideProps() {
-  const conn = await getConnection();
-  const data = await conn.query("SELECT * FROM posts");
-  const posts = data[0];
-
-  return {
-    props: {
-      posts: (posts as []).map((post: Post) => ({
-        id: post.id,
-        text: post.text,
-        author: post.author,
-        created_at: post.created_at.toString(),
-      })),
-    },
-  };
-}
-
-export default function Home({ posts }: { posts: Post[] }) {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {posts.map((post: any) => (
-        <div key={post.id}>{post.text}</div>
-      ))}
+      Tech Tinkers Twitter
     </main>
   );
 }
