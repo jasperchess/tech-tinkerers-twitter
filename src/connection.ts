99df5c1bf25c39224bf729e2088726580bb6be2d
Jasper Chesselet <jasper.chesselet@zappistore.com>
2023-05-08 21:22:02 +0200

    format with prettier

--- src/connection.ts
+++ src/connection.ts
@@ -1,10 +1,10 @@
-import mysql from 'mysql2/promise'
+import mysql from "mysql2/promise";
 
-let connection: mysql.Connection
+let connection: mysql.Connection;
 
 export const getConnection = async () => {
-    if (connection) return connection
+  if (connection) return connection;
 
-    connection = await mysql.createConnection(process.env.DATABASE_URL as string)
-    return connection
-}
+  connection = await mysql.createConnection(process.env.DATABASE_URL as string);
+  return connection;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,33 @@
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-import "./globals.css"
+import "./globals.css";
 import Head from "next/head";
 
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-    getLayout?: (page: ReactElement) => ReactNode;
-};
-   
-type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout;
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
-const inter = Inter({ subsets: ['latin'] })
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Tech Tinkerers Twitter',
-}
+  title: "Tech Tinkerers Twitter",
+};
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-    return (
-        <>
-        <Head>
-            <title>{metadata.title}</title>
-            <link rel="shortcut icon" href="/favicon.ico" />
-        </Head>
-            <Component {...pageProps} />
-        </>
-    );
-  }
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
